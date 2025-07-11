c24cd79acc53e0294fb364ac2d144e46fedca370
Guidix <guidixm@gmail.com>
2025-07-11 04:49:01 +0000

    sapphire: Build FastCharge instead of lineage fastcharge
    - Add FastCharge to PRODUCT_PACKAGES.
    - Address FastCharge related SEpolicy rules.
    - Define sysfs_fastcharge as a domain.
    - Setup permission of fast charging node at boot.
    - Setup Fast Charging node using RRO overlay
    - Drop Lineage fastcharge related policies
    
    Co-authored-by: cyberknight777 <cyberknight755@gmail.com>

--- common.mk
+++ common.mk
@@ -426,6 +426,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    FastChargeSapphire \
     FrameworksResCommon \
     FrameworksResTarget \
     SystemUIResCommon \
